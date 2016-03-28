c5411e1494f94d8a7c5fc0f362336e15190a19fb
YanBo <harry0514s@gmail.com>
2016-03-28 22:50:21 +0800

    add login page

--- PTR/app/src/main/java/asia/tatsujin/ptr/LoginFragment.java
+++ PTR/app/src/main/java/asia/tatsujin/ptr/LoginFragment.java
@@ -1,15 +1,22 @@
 package asia.tatsujin.ptr;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class LoginFragment extends Fragment {
+    String mUsername,mPassword;
+
+    View v;
+    Button getInfo;
 
     public LoginFragment() {
     }
@@ -17,6 +24,26 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        v=inflater.inflate(R.layout.fragment_login, container, false);
+        getInfo=(Button) v.findViewById(R.id.login);
+
+        getInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserInfo();
+            }
+        });
+        return v;
+    }
+
+    public void getUserInfo(){
+        EditText user= (EditText) getView().findViewById(R.id.username);
+        EditText password=(EditText) getView().findViewById(R.id.password);
+
+        mUsername=user.getText().toString();
+        mPassword=password.getText().toString();
+
+        Log.v("login test",mUsername);
+        Log.v("login test",mPassword);
     }
 }
--- PTR/app/src/main/java/asia/tatsujin/ptr/MainActivity.java
+++ PTR/app/src/main/java/asia/tatsujin/ptr/MainActivity.java
@@ -1,11 +1,8 @@
 package asia.tatsujin.ptr;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
 
     @Override
@@ -40,4 +38,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
